17e3521a5a30b832f8f753c799d0292464044edf
AzharrLubiss <azharlubis0507@gmail.com>
2026-07-29 11:13:44 +0700

    perbaikan lagi

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Widgets\PendapatanChart;
+use App\Filament\Widgets\StatistikLaundry;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -39,6 +41,8 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\Filament\Widgets')
             ->widgets([
+                StatistikLaundry::class,
+                PendapatanChart::class,
             ])
             ->middleware([
                 EncryptCookies::class,
